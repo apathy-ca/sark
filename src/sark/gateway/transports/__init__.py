9b590e34b4a43ac445ddbc94c76330db1fdd933e
James Henry <james.henry@telus.com>
2025-12-09 19:01:21 -0500

    fix: Remove stdio import from HTTP/SSE branch __init__.py

--- src/sark/gateway/transports/__init__.py
+++ src/sark/gateway/transports/__init__.py
@@ -1,7 +1,6 @@
-"""Gateway transport implementations for HTTP, SSE, and stdio."""
+"""Gateway transport implementations for HTTP and SSE."""
 
 from sark.gateway.transports.http_client import GatewayHTTPClient
 from sark.gateway.transports.sse_client import GatewaySSEClient
-from sark.gateway.transports.stdio_client import StdioTransport
 
-__all__ = ["GatewayHTTPClient", "GatewaySSEClient", "StdioTransport"]
+__all__ = ["GatewayHTTPClient", "GatewaySSEClient"]
